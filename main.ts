6619c9dd06ff83ac6c2a3dc62d5c8bb9eee36635
juliobeti <132546465+juliobeti@users.noreply.github.com>
2023-05-05 12:15:46 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+basic.showString("¡Hola!")
+for (let index = 0; index < 5; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+}
+basic.clearScreen()
+basic.showString("¡Adios!")
